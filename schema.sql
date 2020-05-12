5a698eaf70ca6172dca3c1d210571dd582597d8e
github username <github email address>
2020-05-11 23:24:33 -0400

    made tables in schema page

---
+++ schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+-- table for all employees:
+CREATE TABLE employees (
+  id INTEGER AUTO_INCREMENT,
+  first_name VARCHAR(50),
+  last_name VARCHAR (50),
+  role_id INTEGER (200),
+  manager_id INTEGER (10) NULL,
+  PRIMARY KEY (id)
+);
+
+-- table for roles
+CREATE TABLE roles (
+  id INTEGER AUTO_INCREMENT,
+  title VARCHAR(50),
+  salary DECIMAL (30.2),
+  department_id INTEGER (10),
+  PRIMARY KEY (id)
+);
+
+-- table for departments
+CREATE TABLE departments (
+  id INTEGER AUTO_INCREMENT,
+  name VARCHAR(50),
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM employees;
+SELECT * FROM roles;
+SELECT * FROM departments;

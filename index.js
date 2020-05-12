55a5e290b53ac78444d5f3187cd4cf780d442489
github username <github email address>
2020-05-12 19:53:35 -0400

    added inquirer.propmt function and updated with questions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require ('inquirer'); 
+const consoleTable = require("console.table")
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3300,
@@ -13,3 +14,68 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId + "\n");
   start(); 
 });
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  init(); 
+});
+
+function init(){
+  inquirer.prompt ([
+    {
+      type: "list", 
+      message: "What would you like to do?",
+      name: "start",
+      choices: [
+      "Add New Employee", 
+      "View all Employees", 
+      "Remove an Employee",
+      "Add a Department", 
+      "View all Departments",
+      "Add Roles", 
+      "View all Roles", 
+      "Update Employee Role", 
+      "Exit"
+    ]}
+  ]).then (function(response){
+      switch (response.init){
+  
+        case "Add New Employee":
+        addNewEmployee();
+        break;
+       
+        case "View all Employees":
+        viewAllEmployees();
+        break; 
+  
+        case "Remove an Employee": 
+        removeEmployee(); 
+        break;
+      
+        case "Add a Department": 
+        addDepartment(); 
+        break;
+  
+        case "View all Departments":
+        viewAllDepartments();
+        break;
+  
+        case "Add Roles": 
+        addRole(); 
+        break;
+  
+        case "View all Roles": 
+        viewAllRoles(); 
+        break;
+      
+        case "Update Employee Role":
+        updateRole(); 
+        break;
+  
+        case "Exit":
+        connection.end(); 
+        break; 
+      }
+    })
+  }

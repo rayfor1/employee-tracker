60ba1d13447107cd7cd2c0967dd155f00c47ea66
github username <github email address>
2020-05-12 19:23:14 -0400

    updated schema.sql table

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ USE employee_db;
 -- table for all employees:
 
 CREATE TABLE employees (
-  id INTEGER AUTO_INCREMENT,
+  id INTEGER NOT NULL,
   first_name VARCHAR(50),
   last_name VARCHAR (50),
   role_id INTEGER (200),
@@ -15,16 +15,16 @@ CREATE TABLE employees (
 
 -- table for roles
 CREATE TABLE roles (
-  id INTEGER AUTO_INCREMENT,
+  id INTEGER NOT NULL,
   title VARCHAR(50),
-  salary DECIMAL (30.2),
+  salary DECIMAL(30,2),
   department_id INTEGER (10),
   PRIMARY KEY (id)
 );
 
 -- table for departments
 CREATE TABLE departments (
-  id INTEGER AUTO_INCREMENT,
+  id INTEGER NOT NULL,
   name VARCHAR(50),
   PRIMARY KEY (id)
 );
